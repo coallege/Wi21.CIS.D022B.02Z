5615ccf032b8541194f43b3c6f3ea44935b5c696
coalpha <22552895+coalpha@users.noreply.github.com>
2021-01-03 23:08:46 -0800

    lab 2.1 done
    I sure love paying for what I use. haha! hahaHAhah!
    AAAAAAAAAAAAAAAAAAAA

--- labs/02/2.1.cxx
+++ labs/02/2.1.cxx
@@ -1,17 +1,21 @@
 #include <array>
+#include <cstddef>
+#include <exception>
 #include <string>
 #include <iostream>
 #include <functional>
 #include <string_view>
 using namespace std;
 
-using IATA = array<char const, 3>;
-
 struct Freight {
-	enum class Type: char {
+	enum class Type: int {
 		Container = 0b010,
 		Pallet = 0b100,
 	};
+
+	static char const * type_to_string(Type t) {
+		return t == Type::Container ? "Container" : "Pallet";
+	}
 	// AYF12345IB, for example
 
 	Type uld;
@@ -19,9 +23,9 @@ struct Freight {
 	// we're still waiting for C++20 constexpr strings -_-
 	string aircraft;
 	int weight;
-	IATA destination;
+	string destination;
 	explicit Freight(
-		Type type, string id, string aircraft, int weight, IATA dest
+		Type type, string id, string aircraft, int weight, string dest
 	) noexcept:
 		uld(type),
 		uldid(id),
@@ -48,41 +52,39 @@ auto prompt(char const *question, fn_t const &&fn) -> T {
 	while (true) {
 		cout << question << "\n> ";
 		cin.clear();
-		cin.ignore();
 		string line;
 		getline(cin, line);
 		try {
 			return actual_fn(line);
-		} catch (...) {
-			cerr << "Invalid input! Try again.\n";
-			continue;
-		}
+		} catch (exception const &ex) {
+			cerr << ex.what() << "!\n\n";
+		} catch (...) {}
 	}
 }
 
-char id_type(string s) {
+int id_type(string s) {
 	if (s.length() != 10) {
-		throw 0;
+		throw exception("Length is too short");
 	}
 
 	auto first3{s.substr(0, 3)};
 
 	if (false
-		|| s == "AYF"
-		|| s == "AYK"
-		|| s == "AAA"
-		|| s == "AYY"
+		|| first3 == "AYF"
+		|| first3 == "AYK"
+		|| first3 == "AAA"
+		|| first3 == "AYY"
 	) {
 		// it's MY gun and I'm pointing it at MY foot
-		return static_cast<char>(Freight::Type::Container);
+		return static_cast<int>(Freight::Type::Container);
 	}
 
 	if (false
-		|| s == "PAG"
-		|| s == "PMC"
-		|| s == "PLA"
+		|| first3 == "PAG"
+		|| first3 == "PMC"
+		|| first3 == "PLA"
 	) {
-		return static_cast<char>(Freight::Type::Pallet);
+		return static_cast<int>(Freight::Type::Pallet);
 	}
 
 	return 0;
@@ -103,7 +105,7 @@ Freight *input() noexcept {
 				if (s == "pallet") {
 					return Freight::Type::Pallet;
 				}
-				throw 0;
+				throw exception("Invalid selection");
 			}
 		)
 	};
@@ -112,8 +114,10 @@ Freight *input() noexcept {
 		prompt<string>(
 			"Enter an appropriate 10 character id.",
 			[type](string s){
-				if (static_cast<char>(type) != id_type(s)) {
-					cerr << "Inappropriate id " << s << " for type.\n";
+				if (static_cast<int>(type) != id_type(s)) {
+					cerr
+						<< "Inappropriate id " << s << " for type "
+						<< Freight::type_to_string(type) << "!\n\n";
 					throw 0;
 				}
 				return move(s);
@@ -123,7 +127,7 @@ Freight *input() noexcept {
 
 	auto aircraft{
 		prompt<string>(
-			"Enter the aircraft that will be carrying the freight.\n",
+			"Enter the aircraft that will be carrying the freight.",
 			[](string s) { return move(s); }
 		)
 	};
@@ -134,8 +138,7 @@ Freight *input() noexcept {
 			[](string s) {
 				auto w{stoi(s)};
 				if (w < 0) {
-					cerr << "Weight must be greater than zero!\n";
-					throw 0;
+					throw exception("Weight must be greater than zero");
 				}
 				return w;
 			}
@@ -143,17 +146,25 @@ Freight *input() noexcept {
 	};
 
 	auto dest{
-		prompt<IATA>(
+		prompt<string>(
 			"Enter the destination of the freight.",
 			[](string s) {
 				if (s.length() != 3) {
-					cerr << "The destination must be an IATA!\n";
-					throw 0;
+					throw exception("The destination must be three characters");
 				}
-				return array{s[0], s[1], s[2]};
+				return move(s);
 			}
 		)
 	};
 
 	return new Freight(type, id, aircraft, weight, dest);
+}
+
+void output(Freight *freight) noexcept {
+	cout << ""
+		"uld         = " << Freight::type_to_string(freight->uld) << "\n"
+		"uldid       = " << freight->uldid << "\n"
+		"aircraft    = " << freight->aircraft << "\n"
+		"weight      = " << freight->weight << "\n"
+		"destination = " << freight->destination << "\n";
 }
