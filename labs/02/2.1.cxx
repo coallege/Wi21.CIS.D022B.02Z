c6ab66be24999ea93eb4de547dc9c36aad68925a
coalpha <22552895+coalpha@users.noreply.github.com>
2021-01-03 23:19:33 -0800

    lab2.1 cleanup

--- labs/02/2.1.cxx
+++ labs/02/2.1.cxx
@@ -8,14 +8,10 @@
 using namespace std;
 
 struct Freight {
-	enum class Type: int {
-		Container = 0b010,
-		Pallet = 0b100,
+	enum class Type {
+		Container,
+		Pallet,
 	};
-
-	static char const * type_to_string(Type t) {
-		return t == Type::Container ? "Container" : "Pallet";
-	}
 	// AYF12345IB, for example
 
 	Type uld;
@@ -35,19 +31,49 @@ struct Freight {
 	{};
 };
 
-// Arrange the functions in the order listed above.
-// so it says...
-Freight *input() noexcept;
-void output(Freight *) noexcept;
+// it said main function first
+inline Freight *input() noexcept;
+inline void output(Freight *) noexcept;
 
 int main() noexcept {
 	Freight *freight{input()};
 	output(freight);
-	// input |> output |> ignore
+}
+
+constexpr char const *to_string(Freight::Type t) noexcept {
+	return t == Freight::Type::Container ? "Container" : "Pallet";
+}
+
+inline Freight::Type id_type(string s) {
+	if (s.length() != 10) {
+		throw exception("Length is too short");
+	}
+
+	auto first3{s.substr(0, 3)};
+
+	if (false
+		|| first3 == "AYF"
+		|| first3 == "AYK"
+		|| first3 == "AAA"
+		|| first3 == "AYY"
+	) {
+		// it's MY gun and I'm pointing it at MY foot
+		return Freight::Type::Container;
+	}
+
+	if (false
+		|| first3 == "PAG"
+		|| first3 == "PMC"
+		|| first3 == "PLA"
+	) {
+		return Freight::Type::Pallet;
+	}
+
+	throw exception("Invalid abbreviation");
 }
 
 template <class T, class fn_t>
-auto prompt(char const *question, fn_t const &&fn) -> T {
+inline auto prompt(char const *question, fn_t const &&fn) noexcept -> T {
 	function<T(string)> actual_fn{fn};
 	while (true) {
 		cout << question << "\n> ";
@@ -62,35 +88,7 @@ auto prompt(char const *question, fn_t const &&fn) -> T {
 	}
 }
 
-int id_type(string s) {
-	if (s.length() != 10) {
-		throw exception("Length is too short");
-	}
-
-	auto first3{s.substr(0, 3)};
-
-	if (false
-		|| first3 == "AYF"
-		|| first3 == "AYK"
-		|| first3 == "AAA"
-		|| first3 == "AYY"
-	) {
-		// it's MY gun and I'm pointing it at MY foot
-		return static_cast<int>(Freight::Type::Container);
-	}
-
-	if (false
-		|| first3 == "PAG"
-		|| first3 == "PMC"
-		|| first3 == "PLA"
-	) {
-		return static_cast<int>(Freight::Type::Pallet);
-	}
-
-	return 0;
-}
-
-Freight *input() noexcept {
+inline Freight *input() noexcept {
 	// when you
 	// when you can't use fast_io to take input from the user
 
@@ -117,7 +115,7 @@ Freight *input() noexcept {
 				if (static_cast<int>(type) != id_type(s)) {
 					cerr
 						<< "Inappropriate id " << s << " for type "
-						<< Freight::type_to_string(type) << "!\n\n";
+						<< to_string(type) << "!\n\n";
 					throw 0;
 				}
 				return move(s);
@@ -160,9 +158,9 @@ Freight *input() noexcept {
 	return new Freight(type, id, aircraft, weight, dest);
 }
 
-void output(Freight *freight) noexcept {
+inline void output(Freight *freight) noexcept {
 	cout << ""
-		"uld         = " << Freight::type_to_string(freight->uld) << "\n"
+		"uld         = " << to_string(freight->uld) << "\n"
 		"uldid       = " << freight->uldid << "\n"
 		"aircraft    = " << freight->aircraft << "\n"
 		"weight      = " << freight->weight << "\n"
