f5a79f96f1f57ffd41b330c8bd3682707b27b58c
coalpha <22552895+coalpha@users.noreply.github.com>
2021-01-07 12:59:08 -0800

    rot13 submitted again

--- lab01/rot13.cxx
+++ lab01/rot13.cxx
@@ -13,33 +13,93 @@ using str = char const *;
 /// Mutable string
 using mut_str = char *;
 
-/// Should be consteval but can't use that yet
-/// I probably also shouldn't be overriding cstdlib names but whatever
-/// I want this to sometimes run at compile time so I gotta do what I gotta do.
-constexpr size_t strlen(str str) noexcept {
-	size_t i{};
-	while (*str++ != '\0') {
-		i++;
-	}
-	return i;
-}
+// in-house string functions
+constexpr bool streq(str a, str b) noexcept;
+constexpr size_t strlen(str str) noexcept;
 
-constexpr bool streq(str a, str b) noexcept {
-	while (*a && *b) {
-		if (*a++ != *b++) {
-			return false;
+// in-house io
+inline void print(str str) noexcept;
+inline mut_str getline() noexcept;
+
+/// Core rot13 algo
+constexpr char rot13_c(char c) noexcept;
+/// Does rot13 in place
+constexpr void rot13(mut_str str) noexcept;
+inline void rot13interactive() noexcept;
+/// Never run at runtime, for testing only
+/// Tests if the rot13 of str a is str b
+constexpr bool rot13_eq(str a, str b) noexcept;
+
+int main(int, char const *argv[]) noexcept {
+	// menu
+	printf(
+		"Cole Gannon's %s\n"
+		"Options:\n"
+		"   E,e,Encode,encode: Take input and output the ROT13 of it.\n"
+		"   D,d,Decode,decode: Exactly the same thing as encode.\n"
+		"   Everything else  : Quit\n"
+		, strlen(argv[0]) > 20 ? "ROT13" : argv[0]
+	);
+
+	// unfortunate, printf flushes stdout and we do it again...
+	for(;;) {
+		print("OPTION> ");
+
+		// remember to delete before leaving scope!
+		mut_str choice = getline();
+
+		if (choice == nullptr) {
+			// If this is null, it's probably because the user hit ctrl+c
+			// we can let them off with a zero error code
+			return 0;
 		}
-	}
-	return true;
-}
 
-/// can return nullptr on error
-/// caller owns memory, remember to delete[]
-inline mut_str getline() noexcept {
-	static const size_t buf_size = 256;
-	// wow, I sure hope nobody ever decides to enter more than 256 characters
-	char *buf = new char[buf_size];
-	return fgets(buf, buf_size, stdin);
+		// I am never going to remember to free this on all paths
+		// so I am going to use RAII to do it for me.
+		size_t len = strlen(choice);
+		// let's slice off the newline at the end
+		size_t last_index = len - 1;
+		choice[last_index] = '\0';
+		// and since we have, let's adjust len
+		len--;
+
+		if (len == 0) {
+			return 0;
+		}
+
+		// let's just make it lowercase so it's easier
+		switch (choice[0]) {
+			case 'E': choice[0] = 'e'; break;
+			case 'D': choice[0] = 'd'; break;
+		}
+
+		if (len == 1) {
+			switch (choice[0]) {
+				case 'e':
+				case 'd':
+					delete[] choice;
+					rot13interactive();
+					continue;
+			}
+		}
+
+		if (len == strlen("encode")) {
+			// luckily for me, encode and decode are the same number of characters
+			// that means that if the string is anything that's not this
+			// I already know that I don't have an option for that user input.
+			// the only valid options are encode and decode.
+			if (false
+				|| streq(choice, "encode")
+				|| streq(choice, "decode")
+			) {
+				delete[] choice;
+				rot13interactive();
+				continue;
+			}
+		}
+
+		return 0;
+	}
 }
 
 // Better pray that the compiler inlines this puppy
@@ -62,20 +122,11 @@ constexpr char rot13_c(char c) noexcept {
 	return c;
 }
 
-/// does rot 13 in place
 constexpr void rot13(mut_str str) noexcept {
 	while (*str != '\0') {
 		*str = rot13_c(*str);
 		str++;
 	}
-}
-
-/// we can be sure that this is flushed
-inline void print(str str) noexcept {
-	// this could fail
-	// too bad!
-	fwrite(str, sizeof(char), strlen(str), stdout);
-	fflush(stdout);
 }
 
 inline void rot13interactive() noexcept {
@@ -95,93 +146,14 @@ inline void rot13interactive() noexcept {
 	delete[] line;
 }
 
-// Why does this exist?
-// Well, that's because if I have a pointer to memory that I own but I have
-// complex control flow, I might forget to free the memory which would cause
-// a leak. Using RAII, I can completely prevent this in a specific scenario.
-struct DeferDelete {
-	str data;
-	DeferDelete(str data) noexcept: data(data) {};
-	~DeferDelete() {
-		delete[] this->data;
-	}
-};
-
-int main(int, char const *argv[]) noexcept {
-	if (strlen(argv[0]) > 20) {
-		argv[0] = "ROT13";
-	}
-
-	printf(
-		"Cole Gannon's %s\n"
-		"Options:\n"
-		"   E,e,Encode,encode: Take input and output the ROT13 of it.\n"
-		"   D,d,Decode,decode: Exactly the same thing as encode.\n"
-		"   Everything else  : Quit\n"
-		, argv[0]
-	);
-	// unfortunate, printf flushes stdout and we do it again...
-
-	for(;;) {
-		fputs("OPTION> ", stdout);
-
-		// I'm glad it's mutable because we're about to do a lot of mutating
-		mut_str choice = getline();
-
-		if (choice == nullptr) {
-			// If this is null, it's probably because the user hit ctrl+c
-			// we can let them off with a zero error code
-			return 0;
-		}
-
-		// I am never going to remember to free this on all paths
-		// so I am going to use RAII to do it for me.
-		DeferDelete on(choice);
-		size_t len = strlen(choice);
-		// let's slice off the newline at the end
-		size_t last_index = len - 1;
-		choice[last_index] = '\0';
-		// and since we have, let's adjust len
-		len--;
-
-		if (len == 0) {
-			return 0;
-		}
-
-		// let's just make it lowercase so it's easier
-		switch (choice[0]) {
-			case 'E': choice[0] = 'e'; break;
-			case 'D': choice[0] = 'd'; break;
-		}
-
-		if (len == 1) {
-			switch (choice[0]) {
-				case 'e':
-				case 'd':
-					rot13interactive();
-					continue;
-			}
-		}
-
-		if (len == strlen("encode")) {
-			// luckily for me, encode and decode are the same number of characters
-			// that means that if the string is anything that's not this
-			// I already know that I don't have an option for that user input.
-			// the only valid options are encode and decode.
-			if (false
-				|| streq(choice, "encode")
-				|| streq(choice, "decode")
-			) {
-				rot13interactive();
-				continue;
-			}
-		}
-
-		return 0;
-	}
+/// Always calls fflush after fwrite
+inline void print(str str) noexcept {
+	// this could fail
+	// too bad!
+	fwrite(str, sizeof(char), strlen(str), stdout);
+	fflush(stdout);
 }
 
-/* TESTS */
 constexpr bool rot13_eq(str a, str b) noexcept {
 	size_t len = strlen(a);
 	size_t blen = strlen(b);
@@ -199,9 +171,35 @@ constexpr bool rot13_eq(str a, str b) noexcept {
 	return true;
 }
 
+/// can return nullptr on error
+/// caller owns memory, remember to delete[]
+inline mut_str getline() noexcept {
+	static const size_t buf_size = 256;
+	// wow, I sure hope nobody ever decides to enter more than 256 characters
+	char *buf = new char[buf_size];
+	return fgets(buf, buf_size, stdin);
+}
+
+constexpr bool streq(str a, str b) noexcept {
+	while (*a && *b) {
+		if (*a++ != *b++) {
+			return false;
+		}
+	}
+	return true;
+}
+
+constexpr size_t strlen(str str) noexcept {
+	size_t i{};
+	while (*str++ != '\0') {
+		i++;
+	}
+	return i;
+}
+
 static_assert(rot13_eq("ALPHABET", "NYCUNORG"));
 static_assert(rot13_eq("NYCUNORG", "ALPHABET"));
-static_assert(rot13_eq("TAF VF", "GNS IS"));
-static_assert(rot13_eq("paddrpf", "cnqqecs"));
-static_assert(rot13_eq("zvpebor", "microbe"));
-static_assert(rot13_eq(" ,.?!", " ,.?!"), "Ignores space and punct");
+static_assert(rot13_eq("TAF VF"  , "GNS IS"  ));
+static_assert(rot13_eq("paddrpf" , "cnqqecs" ));
+static_assert(rot13_eq("zvpebor" , "microbe" ));
+static_assert(rot13_eq(" ,.?!"   , " ,.?!"   ));
