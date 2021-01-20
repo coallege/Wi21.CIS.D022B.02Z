db6810dd15f2726599236b7efbae44ea4a6aafcd
coalpha <22552895+coalpha@users.noreply.github.com>
2021-01-19 21:40:49 -0800

    done in a "normal" way
    I used iostream, :scringe:

---
+++ lab01/README.md
@@ -0,0 +1,3 @@
+# rot13
+
+<img src="../misc/scringe.png" width="300px">
---
+++ lab01/rot13_iostream.cxx
@@ -0,0 +1,63 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string askEncrypt();
+
+void input(string *input);
+void rot13(string *original);
+void output(string *output);
+
+int main() {
+	while (true) {
+		string whatToDo = askEncrypt();
+		if (whatToDo == "encrypt" || whatToDo == "decrypt") {
+			string *inputString = new string;
+
+			input (inputString);
+			rot13 (inputString);
+			output(inputString);
+
+			delete inputString;
+			inputString = nullptr;
+
+			continue;
+		}
+		break;
+	}
+}
+
+string askEncrypt() {
+	cout << "Options:\nencrypt, decrypt, quit [default]\n> ";
+	string option;
+	getline(cin, option);
+	return option;
+}
+
+void input(string *input) {
+	cout << "Input string to be encrypted/decrypted\n> ";
+	getline(cin, *input);
+}
+
+void rot13(string *strptr) {
+	string &input = reinterpret_cast<string &>(*strptr);
+	int inputSize = input.size();
+	int index = 0;
+	while(index != inputSize) {
+		if (input[index] >= 97 && input[index] <= 109) {
+			input[index] = input[index] + 13;
+		} else if (input[index] >= 110 && input[index] <= 122) {
+			input[index] = input[index] - 13;
+		} else if(input[index] >= 65 && input[index] <= 77) {
+			input[index] = input[index] + 13;
+		} else if(input[index] >= 78 && input[index] <= 90) {
+			input[index] = input[index] - 13;
+		}
+		index++;
+	}
+}
+
+void output(string *output) {
+	cout << "Your encrypted/decrypted string is\n> " << *output << "\n\n";
+}
